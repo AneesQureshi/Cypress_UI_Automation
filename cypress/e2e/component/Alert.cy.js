a536e8d776126d76b4d20d0a5faed4c75fdbdbc3
Anish Qureshi <muhammadanish2011@gmail.com>
2026-02-08 22:00:16 +0530

    main Updated

--- cypress/e2e/component/Alert.cy.js
+++ cypress/e2e/component/Alert.cy.js
@@ -36,6 +36,7 @@ describe('DemoQA Alerts handling', () => {
     AlertPage.alertButton.click();
     cy.get('@alertStub').should('have.been.calledOnceWith', 'You clicked a button');
     cy.log("Hello")
+    cy.log("mainAdded")
   });
   
 });
